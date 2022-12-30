0c228aebfe6ac2b1271906278ffc745431ba9d98
BuildTools <unconfigured@null.spigotmc.org>
2022-12-30 12:22:10 -0600

    Cleaned up onPlayerClickListener code

--- src/main/java/de/gurkenwerfer/scratchofftickets/listeners/onPlayerClickListener.java
+++ src/main/java/de/gurkenwerfer/scratchofftickets/listeners/onPlayerClickListener.java
@@ -1,6 +1,7 @@
 package de.gurkenwerfer.scratchofftickets.listeners;
 
 import de.gurkenwerfer.scratchofftickets.ScratchOffTickets;
+import de.gurkenwerfer.scratchofftickets.models.Rarity;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.Material;
@@ -22,224 +23,74 @@ public class onPlayerClickListener implements Listener {
         this.plugin = plugin;
     }
 
+    public enum RewardType {
+        LOOT("Loot"),
+        JACKPOT("Jackpot");
+        private final String name;
+        RewardType(String name) {
+            this.name = name;
+        }
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    public void generateLoot(Rarity rarity, ItemStack i, Player p) {
+        Random random = new Random();
+        int randomInt = random.nextInt(100);
+        RewardType rewardType;
+        if (randomInt <= 90) {
+            rewardType = RewardType.LOOT;
+        } else {
+            rewardType = RewardType.JACKPOT;
+        }
+
+        List<String> loot = plugin.getConfig().getConfigurationSection(rarity.toString()).getStringList(rarity.toString() + rewardType);
+        p.playSound(p.getLocation(), "ticket.redeem.1", 0.7f, 1);
+        int index = random.nextInt(loot.size());
+        String item = loot.get(index);
+
+        if (Material.getMaterial(item.toUpperCase()) != null) {
+            ItemStack wonItem = new ItemStack(Material.getMaterial(item.toUpperCase()));
+            p.getInventory().addItem(wonItem);
+            if (rewardType == RewardType.LOOT) {
+                p.sendMessage("You won a " + item + "!");
+            } else {
+                p.sendMessage("You won the JACKPOT and got a " + item + "!");
+            }
+            i.setAmount(i.getAmount() - 1);
+        } else if (item.startsWith("Spoons:")) {
+            String[] split = item.split(":");
+            int amount = Integer.parseInt(split[1]);
+            Economy economy = ScratchOffTickets.getEconomy();
+            EconomyResponse response = economy.depositPlayer(p, amount);
+            if (!response.transactionSuccess())  p.sendMessage("An error occurred: " + response.errorMessage);
+
+            if (rewardType.equals(RewardType.LOOT)) {
+                p.sendMessage("You won " + economy.format(amount) + " " + economy.currencyNamePlural() + "!");
+            } else {
+                p.sendMessage("You won the JACKPOT amount of " + economy.format(amount) + " " + economy.currencyNamePlural() + "!");
+            }
+            i.setAmount(i.getAmount() - 1);
+
+        }
+    }
+
     @EventHandler
     public void onPlayerClick(PlayerInteractEvent e) {
+        //if the item isn't right-clicked and a paper then return;
+        if (e.getItem() == null || e.getItem().getType() != Material.PAPER || e.getAction() != Action.RIGHT_CLICK_AIR) return;
 
-        if (e.getItem() != null && e.getItem().getType() == Material.PAPER && e.getAction() == Action.RIGHT_CLICK_AIR) {
-            ItemMeta ticketMeta = e.getItem().getItemMeta();
-            Player p = e.getPlayer();
-            if (ticketMeta != null && ticketMeta.hasLore()) {
-                e.getPlayer().sendMessage("You redeemed a ticket!");
-                if (ticketMeta.getLore().get(0).contains("Common")) {
-                    Random random = new Random();
-                    int randomInt = random.nextInt(100);
-                    // Pick from Common Loot Table (90% Chance)
-                    if (randomInt <= 90) {
-                        List<String> loot = plugin.getConfig().getConfigurationSection("Common").getStringList("CommonLoot");
-                        p.playSound(p.getLocation(), "ticket.redeem.1", 0.7f, 1);
-                        int index = new Random().nextInt(loot.size());
-                        String item = loot.get(index);
-                        if (Material.getMaterial(item.toUpperCase()) != null) {
-                            ItemStack wonItem = new ItemStack(Material.getMaterial(item.toUpperCase()));
-                            e.getPlayer().getInventory().addItem(wonItem);
-                            e.getPlayer().sendMessage("You won a " + item + "!");
-                            e.getItem().setAmount(e.getItem().getAmount() - 1);
-                        } else if (item.startsWith("Spoons:")) {
-                            String[] split = item.split(":");
-                            int amount = Integer.parseInt(split[1]);
-                            Economy economy = ScratchOffTickets.getEconomy();
-                            EconomyResponse response = economy.depositPlayer(p, amount);
-                            if (response.transactionSuccess()) {
-                                e.getPlayer().sendMessage("You won " + economy.format(amount) + " " + economy.currencyNamePlural() + "!");
-                                e.getItem().setAmount(e.getItem().getAmount() - 1);
-                            } else {
-                                e.getPlayer().sendMessage("An error occurred: " + response.errorMessage);
-                            }
-                        }
-                    // Else pick from Jackpot Table (10% Chance)
-                    } else {
-                        List<String> loot = plugin.getConfig().getConfigurationSection("Common").getStringList("CommonJackpot");
-                        p.playSound(p.getLocation(), "ticket.redeem.1", 0.7f, 1);
-                        int index = new Random().nextInt(loot.size());
-                        String item = loot.get(index);
-                        if (Material.getMaterial(item.toUpperCase()) != null) {
-                            ItemStack wonItem = new ItemStack(Material.getMaterial(item.toUpperCase()));
-                            e.getPlayer().getInventory().addItem(wonItem);
-                            e.getPlayer().sendMessage("You won the JACKPOT and got a " + item + "!");
-                            e.getItem().setAmount(e.getItem().getAmount() - 1);
-                            p.playSound(p.getLocation(), "ticket.win.1", 0.7f, 1);
-                        } else if (item.startsWith("Spoons:")) {
-                            String[] split = item.split(":");
-                            int amount = Integer.parseInt(split[1]);
-                            Economy economy = ScratchOffTickets.getEconomy();
-                            EconomyResponse response = economy.depositPlayer(p, amount);
-                            if (response.transactionSuccess()) {
-                                e.getPlayer().sendMessage("You won the JACKPOT amount of " + economy.format(amount) + " " + economy.currencyNamePlural() + "!");
-                                e.getItem().setAmount(e.getItem().getAmount() - 1);
-                            } else {
-                                e.getPlayer().sendMessage("An error occurred: " + response.errorMessage);
-                            }
-                            p.playSound(p.getLocation(), "ticket.win.1", 0.7f, 1);
-                        }
-                    }
-                } else if (ticketMeta.getLore().get(0).contains("Rare")) {
-                    Random random = new Random();
-                    int randomInt = random.nextInt(100);
-                    // Pick from Common Loot Table (90% Chance)
-                    if (randomInt <= 90) {
-                        List<String> loot = plugin.getConfig().getConfigurationSection("Rare").getStringList("RareLoot");
-                        p.playSound(p.getLocation(), "ticket.redeem.1", 0.7f, 1);
-                        int index = new Random().nextInt(loot.size());
-                        String item = loot.get(index);
-                        if (Material.getMaterial(item.toUpperCase()) != null) {
-                            ItemStack wonItem = new ItemStack(Material.getMaterial(item.toUpperCase()));
-                            e.getPlayer().getInventory().addItem(wonItem);
-                            e.getPlayer().sendMessage("You won a " + item + "!");
-                            e.getItem().setAmount(e.getItem().getAmount() - 1);
-                        } else if (item.startsWith("Spoons:")) {
-                            String[] split = item.split(":");
-                            int amount = Integer.parseInt(split[1]);
-                            Economy economy = ScratchOffTickets.getEconomy();
-                            EconomyResponse response = economy.depositPlayer(p, amount);
-                            if (response.transactionSuccess()) {
-                                e.getPlayer().sendMessage("You won " + economy.format(amount) + " " + economy.currencyNamePlural() + "!");
-                                e.getItem().setAmount(e.getItem().getAmount() - 1);
-                            } else {
-                                e.getPlayer().sendMessage("An error occurred: " + response.errorMessage);
-                            }
-                        }
-                    // Else pick from Jackpot Table (10% Chance)
-                    } else {
-                        List<String> loot = plugin.getConfig().getConfigurationSection("Rare").getStringList("RareJackpot");
-                        p.playSound(p.getLocation(), "ticket.redeem.1", 0.7f, 1);
-                        int index = new Random().nextInt(loot.size());
-                        String item = loot.get(index);
-                        if (Material.getMaterial(item.toUpperCase()) != null) {
-                            ItemStack wonItem = new ItemStack(Material.getMaterial(item.toUpperCase()));
-                            e.getPlayer().getInventory().addItem(wonItem);
-                            e.getPlayer().sendMessage("You won the JACKPOT and got a " + item + "!");
-                            e.getItem().setAmount(e.getItem().getAmount() - 1);
-                            p.playSound(p.getLocation(), "ticket.win.1", 0.7f, 1);
-                        } else if (item.startsWith("Spoons:")) {
-                            String[] split = item.split(":");
-                            int amount = Integer.parseInt(split[1]);
-                            Economy economy = ScratchOffTickets.getEconomy();
-                            EconomyResponse response = economy.depositPlayer(p, amount);
-                            if (response.transactionSuccess()) {
-                                e.getPlayer().sendMessage("You won the JACKPOT amount of " + economy.format(amount) + " " + economy.currencyNamePlural() + "!");
-                                e.getItem().setAmount(e.getItem().getAmount() - 1);
-                            } else {
-                                e.getPlayer().sendMessage("An error occurred: " + response.errorMessage);
-                            }
-                            p.playSound(p.getLocation(), "ticket.win.1", 0.7f, 1);
-                        }
-                    }
-                } else if (ticketMeta.getLore().get(0).contains("Epic")) {
-                    Random random = new Random();
-                    int randomInt = random.nextInt(100);
-                    // Pick from Common Loot Table (90% Chance)
-                    if (randomInt <= 90) {
-                        List<String> loot = plugin.getConfig().getConfigurationSection("Epic").getStringList("EpicLoot");
-                        p.playSound(p.getLocation(), "ticket.redeem.1", 0.7f, 1);
-                        int index = new Random().nextInt(loot.size());
-                        String item = loot.get(index);
-                        if (Material.getMaterial(item.toUpperCase()) != null) {
-                            ItemStack wonItem = new ItemStack(Material.getMaterial(item.toUpperCase()));
-                            e.getPlayer().getInventory().addItem(wonItem);
-                            e.getPlayer().sendMessage("You won a " + item + "!");
-                            e.getItem().setAmount(e.getItem().getAmount() - 1);
-                        } else if (item.startsWith("Spoons:")) {
-                            String[] split = item.split(":");
-                            int amount = Integer.parseInt(split[1]);
-                            Economy economy = ScratchOffTickets.getEconomy();
-                            EconomyResponse response = economy.depositPlayer(p, amount);
-                            if (response.transactionSuccess()) {
-                                e.getPlayer().sendMessage("You won " + economy.format(amount) + " " + economy.currencyNamePlural() + "!");
-                                e.getItem().setAmount(e.getItem().getAmount() - 1);
-                            } else {
-                                e.getPlayer().sendMessage("An error occurred: " + response.errorMessage);
-                            }
-                        }
-                    // Else pick from Jackpot Table (10% Chance)
-                    } else {
-                        List<String> loot = plugin.getConfig().getConfigurationSection("Epic").getStringList("EpicJackpot");
-                        p.playSound(p.getLocation(), "ticket.redeem.1", 0.7f, 1);
-                        int index = new Random().nextInt(loot.size());
-                        String item = loot.get(index);
-                        if (Material.getMaterial(item.toUpperCase()) != null) {
-                            ItemStack wonItem = new ItemStack(Material.getMaterial(item.toUpperCase()));
-                            e.getPlayer().getInventory().addItem(wonItem);
-                            e.getPlayer().sendMessage("You won the JACKPOT and got a " + item + "!");
-                            e.getItem().setAmount(e.getItem().getAmount() - 1);
-                            p.playSound(p.getLocation(), "ticket.win.1", 0.7f, 1);
-                        } else if (item.startsWith("Spoons:")) {
-                            String[] split = item.split(":");
-                            int amount = Integer.parseInt(split[1]);
-                            Economy economy = ScratchOffTickets.getEconomy();
-                            EconomyResponse response = economy.depositPlayer(p, amount);
-                            if (response.transactionSuccess()) {
-                                e.getPlayer().sendMessage("You won the JACKPOT amount of " + economy.format(amount) + " " + economy.currencyNamePlural() + "!");
-                                e.getItem().setAmount(e.getItem().getAmount() - 1);
-                            } else {
-                                e.getPlayer().sendMessage("An error occurred: " + response.errorMessage);
-                            }
-                            p.playSound(p.getLocation(), "ticket.win.1", 0.7f, 1);
-                        }
-                    }
-                } else if (ticketMeta.getLore().get(0).contains("Legendary")) {
-                    Random random = new Random();
-                    int randomInt = random.nextInt(100);
-                    // Pick from Common Loot Table (90% Chance)
-                    if (randomInt <= 90) {
-                        List<String> loot = plugin.getConfig().getConfigurationSection("Legendary").getStringList("LegendaryLoot");
-                        p.playSound(p.getLocation(), "ticket.redeem.1", 0.7f, 1);
-                        int index = new Random().nextInt(loot.size());
-                        String item = loot.get(index);
-                        if (Material.getMaterial(item.toUpperCase()) != null) {
-                            ItemStack wonItem = new ItemStack(Material.getMaterial(item.toUpperCase()));
-                            e.getPlayer().getInventory().addItem(wonItem);
-                            e.getPlayer().sendMessage("You won a " + item + "!");
-                            e.getItem().setAmount(e.getItem().getAmount() - 1);
-                        } else if (item.startsWith("Spoons:")) {
-                            String[] split = item.split(":");
-                            int amount = Integer.parseInt(split[1]);
-                            Economy economy = ScratchOffTickets.getEconomy();
-                            EconomyResponse response = economy.depositPlayer(p, amount);
-                            if (response.transactionSuccess()) {
-                                e.getPlayer().sendMessage("You won " + economy.format(amount) + " " + economy.currencyNamePlural() + "!");
-                                e.getItem().setAmount(e.getItem().getAmount() - 1);
-                            } else {
-                                e.getPlayer().sendMessage("An error occurred: " + response.errorMessage);
-                            }
-                        }
-                    // Else pick from Jackpot Table (10% Chance)
-                    } else {
-                        List<String> loot = plugin.getConfig().getConfigurationSection("Legendary").getStringList("LegendaryJackpot");
-                        p.playSound(p.getLocation(), "ticket.redeem.1", 0.7f, 1);
-                        int index = new Random().nextInt(loot.size());
-                        String item = loot.get(index);
-                        if (Material.getMaterial(item.toUpperCase()) != null) {
-                            ItemStack wonItem = new ItemStack(Material.getMaterial(item.toUpperCase()));
-                            e.getPlayer().getInventory().addItem(wonItem);
-                            e.getPlayer().sendMessage("You won the JACKPOT and got a " + item + "!");
-                            e.getItem().setAmount(e.getItem().getAmount() - 1);
-                            p.playSound(p.getLocation(), "ticket.win.1", 0.7f, 1);
-                        } else if (item.startsWith("Spoons:")) {
-                            String[] split = item.split(":");
-                            int amount = Integer.parseInt(split[1]);
-                            Economy economy = ScratchOffTickets.getEconomy();
-                            EconomyResponse response = economy.depositPlayer(p, amount);
-                            if (response.transactionSuccess()) {
-                                e.getPlayer().sendMessage("You won the JACKPOT amount of " + economy.format(amount) + " " + economy.currencyNamePlural() + "!");
-                                e.getItem().setAmount(e.getItem().getAmount() - 1);
-                            } else {
-                                e.getPlayer().sendMessage("An error occurred: " + response.errorMessage);
-                            }
-                            p.playSound(p.getLocation(), "ticket.win.1", 0.7f, 1);
-                        }
-                    }
-                }
-            }
-        }
+        ItemMeta ticketMeta = e.getItem().getItemMeta();
+
+        if (ticketMeta == null || !ticketMeta.hasLore()) return;
+
+        e.getPlayer().sendMessage("You redeemed a ticket!");
+
+        if (ticketMeta.getLore().get(0).contains("Common")) generateLoot(Rarity.COMMON, e.getItem(), e.getPlayer());
+        else if (ticketMeta.getLore().get(0).contains("Rare")) generateLoot(Rarity.RARE, e.getItem(),e.getPlayer());
+        else if (ticketMeta.getLore().get(0).contains("Epic")) generateLoot(Rarity.EPIC, e.getItem(), e.getPlayer());
+        else if (ticketMeta.getLore().get(0).contains("Legendary")) generateLoot(Rarity.LEGENDARY, e.getItem(),e.getPlayer());
     }
 }
